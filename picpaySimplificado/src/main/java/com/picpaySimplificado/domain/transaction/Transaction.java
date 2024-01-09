9b208629bc8f8f675995d10351950fea75888bff
Camila <camilad.palma@gmail.com>
2024-01-09 14:58:13 -0300

    feat: create Transactional entity

---
+++ picpaySimplificado/src/main/java/com/picpaySimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,34 @@
+package com.picpaySimplificado.domain.transaction;
+
+import com.picpaySimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne //Uma transação só pode ter 1 Sender
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne //Uma transação só pode ter 1 Receiver
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+
+
+
+}
